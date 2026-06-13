c191bfd56a64b3e68bd31b01a59ae1b68a341e7c
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-06-13 09:32:23 +0900

    Use shared URLs in ShiftBuilder config
    apps/shiftbuilderのconfig.jsでshared/js/shiftcore-urls.jsを参照するように変更。
    ShiftBuilderのDashboard/Login導線をsuite共通URL定義から取得できるようにした。

--- apps/shiftbuilder/js/shiftbuilder/config.js
+++ apps/shiftbuilder/js/shiftbuilder/config.js
@@ -1,15 +1,15 @@
 // ===== ShiftBuilder config ここから =====
 
+import { APP_URLS } from "../../../../shared/js/shiftcore-urls.js";
+
 export const SHIFTBUILDER_API_URL =
   "https://script.google.com/macros/s/AKfycbxlWX3iPy6b1LDjKDc91G7jvBHeee4b5kr7o2wBYy859Uv_R-XI9tLzB2Xu6fz4_-5X/exec";
 
-export const DASHBOARD_URL =
-  "https://shiftcorediv-lab.github.io/shiftcore-suite/apps/account-console/";
-
-export const LOGIN_URL =
-  "https://shiftcorediv-lab.github.io/shiftcore-suite/apps/account-console/";
+export const DASHBOARD_URL = APP_URLS.accountConsole;
+export const LOGIN_URL = APP_URLS.accountConsole;
 
 // Firebase設定は既存のAccount Console側と同じ値を使う。
+// ここはAccount ConsoleのfirebaseConfigをそのまま貼る。
 export const firebaseConfig = {
   apiKey: "ここにFirebase apiKey",
   authDomain: "ここにFirebase authDomain",
